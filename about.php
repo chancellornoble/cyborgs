46addd6d2591faf4d6d6942f682849130d44c4b6
Chancellor Noble <53833927+chancellornoble@users.noreply.github.com>
2020-12-17 21:29:53 -0600

    Add files via upload

---
+++ about.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="styles.css">
+  <link rel="shortcut icon" href=img/transparentlogo.png>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Sriracha&display=swap" rel="stylesheet">
+  <title>Robots</title>
+</head>
+
+ 
+  <body>
+  <header>
+ 
+ <nav class="navbar navbar-expand-lg navbar-light bg-light">
+   <a class="navbar-brand" href="index.php"><img class="logoimg" src="img/transparentlogo.png"></a>
+   <!---  <a class="navbar-brand" href="index.php">Shop</a> --->
+   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+     <span class="navbar-toggler-icon"></span>
+   </button>
+   <div class="collapse navbar-collapse" id="navbarSupportedContent">
+     <ul class="navbar-nav mr-auto">
+       <li class="nav-item active">
+         <a class="nav-link" href="index.php">Home</a>
+       </li>
+       <li class="nav-item">
+         <a class="nav-link" href="about.php">About<span class="sr-only">(current)</span></a>
+       </li>
+       <li class="nav-item">
+         <a class="nav-link" href="robots.php">Robot</a>
+       </li>
+       <li class="nav-item">
+         <a class="nav-link" href="other.php">Other</a>
+       </li>
+     </ul>
+   </div>
+ </nav>
+
+ </header>
+
+<h1>About </h1>
+
+<footer class="footercont">
+  <div class="container">
+    <div class="row pt-4 text-center d-flex justify-content-center">
+      <div class="col-md-2 mb-3">
+        <h6 class="title font-bold"><a href="about.php" class="foottext">About</a></h6>
+      </div>
+      <div class="col-md-2 mb-3">
+        <h6 class="title font-bold"><a href="#toppage" class="foottext">Back to Top</a></h6>
+      </div>
+      <div class="col-md-2 mb-3">
+        <h6 class="title font-bold"><a href="robots.php" class="foottext">Robots</a></h6>
+      </div>
+    </div>
+    <hr class="rgba-white-light">
+    <hr class="clearfix d-md-none rgba-white-light">
+    <div class="row pb-3">
+      <div class="col-md-12">
+        
+      </div>
+    </div>
+  </div>
+  <div class="footer-copyright">
+    <div class="container-fluid copyfoot">
+      © 2020
+    </div>
+  </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+      
+  </body>
+</html>
---
+++ orders.php
@@ -0,0 +1,54 @@
+<?php
+ include_once 'inc/connection.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+
+
+
+<?php
+$servername = "localhost:3306";
+$username = "mahs_cnoble";
+$password = "mustangs";
+$dbname = "mahs_cnoble";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO orders (name, height, color, job, gender)
+VALUES ('', '', '', '', '')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Your Order has been created!";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
+<br>
+<br>
+<br>
+<form action="robots.php">
+  <button type="submit" name="submitbutton" value="Back to Robots"></button>
+</form>
+
+
+</body>
+</html>
+
+
